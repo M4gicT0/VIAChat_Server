8abe1187dbaba922bc980576a515905992e24246
Théo Morales <theo.morales.fr@gmail.com>
2016-11-24 17:19:59 +0100

    Added web service

---
+++ VIAChatServer/VIAChatServer/IOnlineUsersService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace VIAChatServer
+{
+    [ServiceContract]
+    public interface IOnlineUsersService
+    {
+        [OperationContract]
+        [WebGet(
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped, 
+            UriTemplate = "online"
+        )]
+        String NumberOfOnlineUsers();
+    }
+}
